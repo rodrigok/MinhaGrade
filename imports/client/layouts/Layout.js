b1ca9dafea3f593c8c7d4d6a62d34eff8bef01e5
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2018-06-06 21:55:06 -0300

    Remove semantic ui && accounts ui

--- imports/client/layouts/Layout.js
+++ imports/client/layouts/Layout.js
@@ -6,11 +6,264 @@ import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
 import {
 	Menu,
-	Layout
+	Layout,
+	Icon,
+	Form,
+	Input,
+	Button,
+	message,
+	Card
 } from 'antd';
 
-
 import { render } from 'react-dom';
+
+class AccountComponent extends Component {
+	static propTypes = {
+		user: PropTypes.object,
+		getFieldDecorator: PropTypes.any,
+		form: PropTypes.any
+	}
+
+	state = {}
+
+	handleLogin = (e) => {
+		e.preventDefault();
+		this.setState({loading: true});
+		this.props.form.validateFields((err, values) => {
+			this.setState({loading: false});
+			if (!err) {
+				Meteor.loginWithPassword({email: values.email}, values.password, (error) => {
+					if (error) {
+						message.error(error.reason);
+					}
+				});
+			}
+		});
+	}
+
+	handleSignup = (e) => {
+		e.preventDefault();
+		this.setState({loading: true});
+		this.props.form.validateFields((err, {email, password}) => {
+			this.setState({loading: false});
+			if (!err) {
+				Accounts.createUser({email, password}, (error) => {
+					if (error) {
+						return message.error(error.reason);
+					}
+
+					Meteor.loginWithPassword({email}, password, (error) => {
+						if (error) {
+							message.error(error.reason);
+						}
+					});
+				});
+			}
+		});
+	}
+
+	handleForgotPassword = (e) => {
+		e.preventDefault();
+		this.setState({loading: true});
+		this.props.form.validateFields((err, {email}) => {
+			this.setState({loading: false});
+			if (!err) {
+				Accounts.forgotPassword({email}, (error) => {
+					if (error) {
+						return message.error(error.reason);
+					}
+
+					message.info('Email enviado');
+					this.setState({
+						action: 'login'
+					});
+				});
+			}
+		});
+	}
+
+	handleChangePassword = (e) => {
+		e.preventDefault();
+		this.setState({loading: true});
+		this.props.form.validateFields((err, {password, newPassword}) => {
+			this.setState({loading: false});
+			if (!err) {
+				Accounts.changePassword(password, newPassword, (error) => {
+					if (error) {
+						return message.error(error.reason);
+					}
+
+					message.info('Senha alterada');
+
+					this.setState({
+						action: ''
+					});
+				});
+			}
+		});
+	}
+
+	renderLogin() {
+		const { getFieldDecorator } = this.props.form;
+		return (
+			<Card title='Entrar'>
+				<Form onSubmit={this.handleLogin} className='login-form'>
+					<Form.Item>
+						{getFieldDecorator('email', {
+							rules: [{ required: true, type: 'email', message: 'Por favor entre com seu email!' }]
+						})(
+							<Input prefix={<Icon type='user' style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder='email' />
+						)}
+					</Form.Item>
+					<Form.Item>
+						{getFieldDecorator('password', {
+							rules: [{ required: true, message: 'Por favor entre com sua senha!' }]
+						})(
+							<Input prefix={<Icon type='lock' style={{ color: 'rgba(0,0,0,.25)' }} />} type='password' placeholder='senha' />
+						)}
+					</Form.Item>
+					<Form.Item>
+						<Button type='primary' htmlType='submit' className='login-form-button' loading={this.state.loading}>
+							Entrar
+						</Button>
+						<a href='' onClick={() => this.setState({action: 'forgot-password'})}>Esqueci minha senha</a>
+						<a href='' className='login-form-register' onClick={() => this.setState({action: 'signup'})}>Criar conta</a>
+					</Form.Item>
+				</Form>
+			</Card>
+		);
+	}
+
+	renderSignup() {
+		const { getFieldDecorator } = this.props.form;
+		return (
+			<Card title='Criar conta'>
+				<Form onSubmit={this.handleSignup} className='login-form'>
+					<Form.Item>
+						{getFieldDecorator('email', {
+							rules: [{ required: true, type: 'email', message: 'Por favor entre com seu email!' }]
+						})(
+							<Input prefix={<Icon type='user' style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder='email' />
+						)}
+					</Form.Item>
+					<Form.Item>
+						{getFieldDecorator('password', {
+							rules: [{ required: true, message: 'Por favor entre com sua senha!' }]
+						})(
+							<Input prefix={<Icon type='lock' style={{ color: 'rgba(0,0,0,.25)' }} />} type='password' placeholder='senha' />
+						)}
+					</Form.Item>
+					<Form.Item>
+						<Button type='primary' htmlType='submit' className='login-form-button' loading={this.state.loading}>
+							Criar conta
+						</Button>
+						<a href='' className='login-form-register' onClick={() => this.setState({action: 'login'})}>Entrar</a>
+					</Form.Item>
+				</Form>
+			</Card>
+		);
+	}
+
+	renderForgotPassword() {
+		const { getFieldDecorator } = this.props.form;
+		return (
+			<Card title='Esqueci minha senha'>
+				<Form onSubmit={this.handleForgotPassword} className='login-form'>
+					<Form.Item>
+						{getFieldDecorator('email', {
+							rules: [{ required: true, type: 'email', message: 'Por favor entre com seu email!' }]
+						})(
+							<Input prefix={<Icon type='user' style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder='email' />
+						)}
+					</Form.Item>
+					<Form.Item>
+						<Button type='primary' htmlType='submit' className='login-form-button' loading={this.state.loading}>
+							Resetar senha
+						</Button>
+						<a href='' className='login-form-register' onClick={() => this.setState({action: 'login'})}>Entrar</a>
+					</Form.Item>
+				</Form>
+			</Card>
+		);
+	}
+
+	renderChangePassword() {
+		const { getFieldDecorator } = this.props.form;
+		return (
+			<Card title='Mudar senha'>
+				<Form onSubmit={this.handleChangePassword} className='login-form'>
+					<Form.Item>
+						{getFieldDecorator('password', {
+							rules: [{ required: true, message: 'Por favor entre com sua senha!' }]
+						})(
+							<Input prefix={<Icon type='lock' style={{ color: 'rgba(0,0,0,.25)' }} />} type='password' placeholder='senha atual' />
+						)}
+					</Form.Item>
+					<Form.Item>
+						{getFieldDecorator('newPassword', {
+							rules: [{ required: true, message: 'Por favor entre com a nova senha!' }]
+						})(
+							<Input prefix={<Icon type='lock' style={{ color: 'rgba(0,0,0,.25)' }} />} type='password' placeholder='nova senha' />
+						)}
+					</Form.Item>
+					<Form.Item>
+						<Button type='primary' htmlType='submit' className='login-form-button' loading={this.state.loading}>
+							Mudar senha
+						</Button>
+						<a href='' className='login-form-register' onClick={() => this.setState({action: ''})}>Cancelar</a>
+					</Form.Item>
+				</Form>
+			</Card>
+		);
+	}
+
+	renderAccount() {
+		const {user} = this.props;
+		let userEmail;
+		if (user && user.admin) {
+			userEmail = user.emails[0].address;
+		}
+
+		return (
+			<Card title={userEmail}>
+				<Form className='login-form'>
+					<Form.Item>
+						<Button onClick={() => this.setState({action: 'change-password'})} className='login-form-button'>
+							Mudar senha
+						</Button>
+						<Button type='primary' onClick={() => Meteor.logout()} className='login-form-button'>
+							Sair
+						</Button>
+					</Form.Item>
+				</Form>
+			</Card>
+		);
+	}
+
+	render() {
+		if (this.props.user) {
+			switch (this.state.action) {
+				case 'change-password':
+					return this.renderChangePassword();
+				default:
+					return this.renderAccount();
+			}
+		}
+
+		switch (this.state.action) {
+			case 'signup':
+				return this.renderSignup();
+			case 'forgot-password':
+				return this.renderForgotPassword();
+			default:
+				return this.renderLogin();
+		}
+	}
+}
+
+const WrappedAccountComponent = Form.create()(AccountComponent);
+
+
 
 class MenuComponent extends Component {
 	static propTypes = {
@@ -24,7 +277,14 @@ class MenuComponent extends Component {
 	}
 
 	handleClick(e) {
-		Router.go(e.key);
+		switch (e.key) {
+			case 'logout':
+				Meteor.logout();
+				break;
+
+			default:
+				Router.go(e.key);
+		}
 	}
 
 	renderAdminMenu() {
@@ -34,22 +294,40 @@ class MenuComponent extends Component {
 		}
 	}
 
+	renderAccounts() {
+		const {user} = this.props;
+		let userEmail = 'Entrar / Criar Conta';
+
+		if (user) {
+			userEmail = user.emails[0].address;
+		}
+
+		return (
+			<Menu.SubMenu title={<span><Icon type='user' />{userEmail}</span>}>
+				<WrappedAccountComponent user={this.props.user} />
+			</Menu.SubMenu>
+		);
+	}
+
 	render() {
 		return (
-			<Layout.Header>
-				<div className='logo' />
-				<Menu
-					theme='light'
-					mode='horizontal'
-					// defaultSelectedKeys={['2']}
-					style={{ lineHeight: '64px' }}
-					onClick={this.handleClick}
-				>
-					<Menu.Item key='/course/si'>Curso</Menu.Item>
-					<Menu.Item key='/calendar/2018-2'>Calendario</Menu.Item>
-					{this.renderAdminMenu()}
-				</Menu>
-			</Layout.Header>
+			<React.Fragment>
+				<Layout.Header>
+					<div className='logo' />
+					<Menu
+						theme='light'
+						mode='horizontal'
+						// defaultSelectedKeys={['2']}
+						style={{ lineHeight: '64px' }}
+						onClick={this.handleClick}
+					>
+						<Menu.Item key='/course/si'>Curso</Menu.Item>
+						<Menu.Item key='/calendar/2018-2'>Calendario</Menu.Item>
+						{this.renderAdminMenu()}
+						{this.renderAccounts()}
+					</Menu>
+				</Layout.Header>
+			</React.Fragment>
 		);
 	}
 }
@@ -63,17 +341,3 @@ const MenuComponentWithTracking = withTracker(() => {
 Template.Layout.onRendered(() => {
 	render(<MenuComponentWithTracking />, document.getElementById('render-menu'));
 });
-
-
-// Template.Layout.helpers({
-// 	isAdmin() {
-// 		return Meteor.user() && Meteor.user().admin === true;
-// 	},
-
-// 	calendars() {
-// 		return Calendar.find();
-// 	}
-// });
-
-
-// Template.Layout.onRendered(() => $('.menu .ui.dropdown').dropdown());
