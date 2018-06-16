9180305f88d431df3fd46574b708ba74b589a2c5
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2018-06-16 11:16:04 -0300

    Use subscriptions without DDP

--- server/api/index.js
+++ server/api/index.js
@@ -1,5 +1,10 @@
-import a, { setupHttpEndpoint, setup as createGraphQLPublication } from 'meteor/swydo:ddp-apollo';
+import { setupHttpEndpoint } from 'meteor/swydo:ddp-apollo';
 import { makeExecutableSchema } from 'graphql-tools';
+import { createServer } from 'http';
+import { SubscriptionServer } from 'subscriptions-transport-ws';
+import { execute, subscribe } from 'graphql';
+
+const WS_PORT = 5000;
 
 import typeDefs from './schema.graphql';
 import { resolvers } from './resolvers';
@@ -9,10 +14,39 @@ const schema = makeExecutableSchema({
 	resolvers
 });
 
-setupHttpEndpoint({
-	schema
+const websocketServer = createServer((request, response) => {
+	response.writeHead(404);
+	response.end();
 });
 
-createGraphQLPublication({
+websocketServer.listen(WS_PORT, () => console.log(
+	`Websocket Server is now running on http://localhost:${ WS_PORT }`
+));
+
+import { getUserIdByLoginToken } from '../getUserByLoginToken';
+
+
+SubscriptionServer.create({
+	schema,
+	execute,
+	subscribe,
+	onConnect: (connectionParams) => {
+		if (connectionParams.authToken) {
+			return getUserIdByLoginToken(connectionParams.authToken)
+				.then((userId) => {
+					return {
+						userId
+					};
+				});
+		}
+
+		throw new Error('Missing auth token!');
+	}
+}, {
+	server: websocketServer,
+	path: '/subscriptions'
+});
+
+setupHttpEndpoint({
 	schema
 });
